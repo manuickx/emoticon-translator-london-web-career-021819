e2b8c3503b0b16b5b6ee841e800b0f80c80b51e7
Manu Ticusan <manuelticusan@yahoo.com>
2019-02-15 19:37:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,9 +6,18 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  emoticons = load_library(file_path)
-  emoji = emoticons.values.find {|faces| faces[0] == emoticon}
-  emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
+emoticons = load_library(file_path)
+emoji = nil
+  emoticons.each do |name, emojis|
+    if emoticon == emojis[0]
+      emoji = emojis[1]
+    end
+  end
+if emoji == nil
+  "Sorry, that emoticon was not found"
+else 
+  emoji
+end
 end
 
 def get_english_meaning(file_path, emoticon)
