a159786b606eca99e47f7c9995f35e1f07e72dbe
Manu Ticusan <manuelticusan@yahoo.com>
2019-02-15 20:24:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,10 +3,10 @@ require "yaml"
 
 def load_library(path)
   emoticons = YAML.load_file(path)
-  emoticon_hash = Hash.new
+  emoticon_hash = {}
 
-  emoticon_hash["get_emoticon"] = Hash.new
-  emoticon_hash["get_meaning"] = Hash.new
+  emoticon_hash["get_emoticon"] = {}
+  emoticon_hash["get_meaning"] = {}
 
   emoticons.each do |english_word, emoticon_set|
     emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
